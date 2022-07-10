45ca92b5573c2e1590dcfcaa1bf118c32708e477
qsus <106612195+qsus@users.noreply.github.com>
2022-07-10 21:39:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# toki-pona-translator
+Simple translator, or rather dictionary for Toki Pona. Designed to be run quickly from your linux start menu.
+## Instalation
+1. Clone the repository `git clone https://github.com/qsus/toki-pona-translator` to your home directory.
+2. Replace "USER" in .desktop file with your username both in Exec and Icon entry
+3. Install script by running `desktop-file-install "Toki Pona Translator.desktop"` (may require sudo). Alternatively, copy the file to `/usr/share/applications`.
---
+++ translator.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import json
+dictionary = json.load(open('toki-pona.json'))
+
+print("\n\nProgram can be stopped at any time by entering nothing.")
+
+def printDef(word):
+	print(f"\n{c.G}{c.BOLD}{word}{c.NOBD}: {dictionary[word]}")
+
+def printOther(word):
+	print(f"{c.O}{word}{c.END}", end="")
+
+class c:
+	G = '\033[92m'
+	O = '\033[93m'
+	R = '\033[91m'
+	B = '\033[94m'
+	BOLD = '\033[1m'
+	NOBD = '\033[22m'
+	END = '\033[0m'
+
+while True:
+	try:
+		phrase = input(f"{c.B}Enter toki pona phrase: {c.END}")
+	except KeyboardInterrupt:
+		break
+
+	# exit
+	if phrase == '':
+		break
+
+	for word in phrase.split():
+		wordNoPunc = word.strip('.,!?:;')
+		if wordNoPunc in dictionary:
+			printDef(wordNoPunc)
+		else:
+			printOther(word)
+
+	print() # newline
+
+
+# on exit
+print(f"{c.B}Goodbye!{c.END}")
